022dac66940671d4d35add68238f422b0495293a
Bialson <bialson95@gmail.com>
2023-05-23 21:47:25 +0200

    splash screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider, ThemeConfig, extendTheme } from '@chakra-ui/react'
 import './index.css'
+import StartupPage from './pages/startup.tsx'
+import theme from './theme/index.ts'
 
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-}
+// const config: ThemeConfig = {
+//   useSystemColorMode: false,
+//   initialColorMode: "dark",
+// }
 
-// 3. extend the theme
-const customTheme = extendTheme({ config })
+// // 3. extend the theme
+// const customTheme = extendTheme({ config })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ChakraProvider theme={customTheme}>
-    <App />
+  <ChakraProvider theme={theme}>
+    <StartupPage />
   </ChakraProvider>
 )
