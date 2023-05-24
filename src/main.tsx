97bd69d794049df715ac316dbf55372c2e6cbdb4
Bialson <bialson95@gmail.com>
2023-05-24 23:08:37 +0200

    routing fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,20 @@ import ReactDOM from 'react-dom/client';
 // import App from './App.tsx'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { StartupPage } from './pages/startup.tsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { ErrorPage } from './pages/error_page.tsx';
 import './index.css';
 import { theme } from './theme/index.ts';
 import { Sheets } from './pages/sheets.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 	{
-		path: '/',
-		errorElement: <ErrorPage />,
-	},
-	{
-		path: '/ee09_inf03/',
+		path: '/*',
 		element: <StartupPage />,
 		errorElement: <ErrorPage />,
 	},
 	{
-		path: '/ee09_inf03/sheets',
+		path: '/sheets',
 		element: <Sheets />,
 		errorElement: <ErrorPage />,
 	},
